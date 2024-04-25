4bead371c94e83c2234db4a8aee3f3c6cdd01d64
EcSid <166507541+EcSid@users.noreply.github.com>
2024-04-25 09:35:47 +0300

    Update level1.js

--- level1.js
+++ level1.js
@@ -2,6 +2,20 @@ const ball = document.querySelector(".pizza");
 const vanyaShd = document.querySelector(".vanyaShd");
 const vanyaEst = document.querySelector(".vanyaEst");
 let bool = false;
+document.onkeydown = function(e) {
+if(event.keyCode == 123) {
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+return false;
+}
+}
 setTimeout(() => document.body.classList.remove("hidden"), 2000)
 
 ball.onmousedown = function(e) { // (1) отследить нажатие
